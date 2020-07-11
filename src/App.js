62921b120fcd9bf3b91848dab032fe990985ecdf
Gyula Horváth <horvath.gyula324@gmail.com>
2020-07-12 01:07:05 +0200

    Made location query form, linked form and API fetch, gave background, changed CSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 import 'weather-icons/css/weather-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+import Form from './components/form.component';
 import Weather from './components/weather.component';
 
 const API_key = 'fd9fb084a09d8b5949e8d196f037950d';
@@ -22,9 +22,8 @@ class App extends React.Component {
       description: "",
       error: false
     };
-    this.getWeather();
 
-    this.weatherIcon={
+    this.weatherIcon = {
       Thunderstorm: "wi-thunderstorm",
       Drizzle: "wi-sleet",
       Rain: "wi-storm-showers",
@@ -35,79 +34,91 @@ class App extends React.Component {
     }
   }
 
-  calCelsius(temp){
+  calCelsius(temp) {
     let celsius = Math.floor(temp - 273.15);
     return celsius;
   }
 
-  getWeatherIcon(icons, id){
-    switch(true){
-      case id >= 200 && id <= 232: 
-      this.setState({
-        icon: this.weatherIcon.Thunderstorm
-      })
-      break;
-      case id >= 300 && id <= 321: 
-      this.setState({
-        icon: this.weatherIcon.Drizzle
-      })
-      break;
-      case id >= 500 && id <= 531: 
-      this.setState({
-        icon: this.weatherIcon.Rain
-      })
-      break;
-      case id >= 600 && id <= 622: 
-      this.setState({
-        icon: this.weatherIcon.Snow
-      })
-      break;
-      case id >= 700 && id <= 781: 
-      this.setState({
-        icon: this.weatherIcon.Atmosphere
-      })
-      break;
-      case id === 800: 
-      this.setState({
-        icon: this.weatherIcon.Clear
-      })
-      break;
-      case id >= 801 && id <= 804: 
-      this.setState({
-        icon: this.weatherIcon.Clouds
-      })
-      break;
+  getWeatherIcon(icons, id) {
+    switch (true) {
+      case id >= 200 && id <= 232:
+        this.setState({
+          icon: this.weatherIcon.Thunderstorm
+        })
+        break;
+      case id >= 300 && id <= 321:
+        this.setState({
+          icon: this.weatherIcon.Drizzle
+        })
+        break;
+      case id >= 500 && id <= 531:
+        this.setState({
+          icon: this.weatherIcon.Rain
+        })
+        break;
+      case id >= 600 && id <= 622:
+        this.setState({
+          icon: this.weatherIcon.Snow
+        })
+        break;
+      case id >= 700 && id <= 781:
+        this.setState({
+          icon: this.weatherIcon.Atmosphere
+        })
+        break;
+      case id === 800:
+        this.setState({
+          icon: this.weatherIcon.Clear
+        })
+        break;
+      case id >= 801 && id <= 804:
+        this.setState({
+          icon: this.weatherIcon.Clouds
+        })
+        break;
       default: this.setState({
         icon: this.weatherIcon.Clouds
       })
-      break;
+        break;
     }
   }
 
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&appid=${API_key}`)
+  getWeather = async (e) => {
 
-    const response = await api_call.json();
+    e.preventDefault();
 
-    console.log(response);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
 
-    this.setState({
-      city: response.name,
-      country: response.sys.country,
-      celsius: this.calCelsius(response.main.temp),
-      temp_min: this.calCelsius(response.main.temp_min),
-      temp_max: this.calCelsius(response.main.temp_max),
-      description: response.weather[0].description
-    })
+    if (city && country) {
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`)
 
-    this.getWeatherIcon(this.weatherIcon, response.weather[0].id)
+      const response = await api_call.json();
+
+      console.log(response);
+
+      this.setState({
+        city: `${response.name}, ${response.sys.country}`,
+        celsius: this.calCelsius(response.main.temp),
+        temp_min: this.calCelsius(response.main.temp_min),
+        temp_max: this.calCelsius(response.main.temp_max),
+        description: response.weather[0].description,
+        error: false
+      })
+
+      this.getWeatherIcon(this.weatherIcon, response.weather[0].id)
+    } else {
+      this.setState({
+        error: true
+      })
+    }
   }
   render() {
     return (
       <div className="App">
+        <Form loadWeather={this.getWeather} error={this.state.error} />
         <Weather
           city={this.state.city}
-          country={this.state.country}
           temp_celsius={this.state.celsius}
           temp_min={this.state.temp_min}
           temp_max={this.state.temp_max}
---
+++ src/components/form.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './form.style.css';
+
+const Form = props => {
+    return (
+        <div className="container">
+            <div>{props.error ? error() : null}</div>
+            <form onSubmit={props.loadWeather}>
+                <div className="row">
+                    <div className="col-md-3 offset-md-2">
+                        <input type="text" name="city" autoComplete="off" className="form-control shadow-none bg-transparent text-white" placeholder="City" />
+                    </div>
+                    <div className="col-md-3">
+                        <input type="text" name="country" autoComplete="off" className="form-control shadow-none bg-transparent text-white" placeholder="Country" />
+                    </div>
+                    <div className="col-md-3 mt-md-0 py-2 text-md-left">
+                        <button className="btn btn-warning">Get Weather</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+function error() {
+    return (
+        <div className="aler alert-danger mx-5" role="alert">
+            Please enter city and country
+        </div>
+    )
+}
+
+export default Form;
